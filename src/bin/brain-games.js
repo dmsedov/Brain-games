509de0aaed6647c9ecd1d03856f3a87e8b540af3
Dmitry Sedov <dmsedov92@gmail.com>
2017-07-18 12:51:44 +0300

    update files

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 import brainGames from '..';
 
-console.log('Welcome to the Brain Games!');
-
 brainGames();
---
+++ src/games/brain-even.js
@@ -0,0 +1,10 @@
+import brainGames from '..';
+
+const task = 'Answer "yes" if number even otherwise answer "no"';
+const brainEven = () => {
+  const randNum = Math.floor(Math.random() * 100);
+  const isEven = randNum % 2 === 0 ? 'yes' : 'no';
+  return { randNum, isEven };
+};
+
+export default () => brainGames(task, brainEven);
