d73399f5ecbef21fea2788116c2ded01ea379b22
Dmitry Sedov <dmsedov92@gmail.com>
2017-07-20 12:13:09 +0300

    add brain-progression.js files

---
+++ src/games/brain-progression.js
@@ -0,0 +1,29 @@
+import { cons } from 'hexlet-pairs';
+import runBrainGame from '..';
+import getRandNum from '../getRandNum';
+
+const task = 'What number is missing in this progression?';
+const brainProgression = () => {
+  const randNum = getRandNum(0, 10);
+  const randStep = getRandNum(0, 9);
+  const randIndex = getRandNum(0, 9);
+  const makeSeq = (num1, num2, acc) => {
+    if (acc.length === 10) {
+      return acc;
+    }
+    const nextNum = num1 + num2;
+    acc.push(nextNum);
+    return makeSeq(nextNum, num2, acc);
+  };
+  const seqOfNum = makeSeq(randNum, randStep, [randNum]);
+  const requiredNum = seqOfNum[randIndex];
+  const newSeqOfNum = seqOfNum.map((element, index) => {
+    if (index === randIndex) {
+      return '..';
+    }
+    return element;
+  }).join(' ');
+  return cons(`${newSeqOfNum}`, `${requiredNum}`);
+};
+
+export default () => runBrainGame(task, brainProgression);
